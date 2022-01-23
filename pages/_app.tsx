0e05745ee2610fb3924a9ebb31003e2bb61ea0a3
waynethebb <wonjun92@gmail.com>
2022-01-23 22:19:22 +1300

    setup routing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,30 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/common/layout';
+import { useRouter } from 'next/router';
+
 const date = new Date();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout date={date}>
-      <Component {...pageProps} />
-    </Layout>
-  );
+  const pathName = useRouter().pathname;
+
+  switch (pathName) {
+    case '/signin':
+      return <Component {...pageProps} />;
+      break;
+
+    case '/':
+      return (
+        <Layout date={date}>
+          <Component {...pageProps} />
+        </Layout>
+      );
+      break;
+
+    default:
+      return <p>error</p>;
+      break;
+  }
 }
 
 export default MyApp;
